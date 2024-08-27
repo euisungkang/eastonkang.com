6f051493e122149060ea25c018083366a2b0afed
euisungkang <euisungkang@gmail.com>
2024-08-27 23:41:34 +0900

    feat: aos integration and mock route added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,74 @@
 <script lang="ts">
 	import sign from "$lib/assets/sign-min.png";
 	import AOS from "aos";
+	import { onMount } from 'svelte';
 
-	AOS.init();
+	onMount(() => {
+		AOS.init();
+	});
 
 	const shapes = [
 		{
-			color: '#8E7AB5'
+			color: '#8E7AB5',
+			href: '/wavy',
+			direction: 'fade-right',
+			delay: 0,
 		},
 		{
-			color: '#B784B7'
+			color: '#B784B7',
+			href: '/wavy',
+			direction: 'fade-down',
+			delay: 100,
 		},
 		{
-			color: '#E493B3'
+			color: '#E493B3',
+			href: '/wavy',
+			direction: 'fade-left',
+			delay: 200,
 		},
 		{
-			color: '#EEA5A6'
+			color: '#EEA5A6',
+			href: '/wavy',
+			direction: 'fade-right',
+			delay: 500,
 		},
 		{
-			color: '#8E7AB5'
+			color: '#8E7AB5',
+			href: '/wavy',
+			direction: 'fade-up',
+			delay: 400,
 		},
 		{
-			color: '#B784B7'
+			color: '#B784B7',
+			href: '/wavy',
+			direction: 'fade-left',
+			delay: 300,
 		},
 	];
 </script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</svelte:head>
 
 <div class="dark h-screen w-screen flex items-center justify-center bg-[#121212]">
 	<div class="w-full h-full flex items-center justify-center">
 		<div class="grid grid-cols-3 place-items-center place-content-center">
 			{#each shapes as shape}
-				<div
-					class="h-52 w-52 col-span-1 m-4 rounded-xl cursor-pointer
-						 transition ease-in-out duration-200 hover:scale-110"
-					style="background-color: {shape.color}"
+				<a
+					href={shape.href}
+					class="transition ease-in-out duration-200 hover:scale-110"
 				>
-				</div>
+					<div
+						data-aos={shape.direction}
+						data-aos-duration={750}
+						data-aos-delay={shape.delay}
+						data-aos-easing="ease-in-out"
+						class="h-64 w-64 col-span-1 m-4 rounded-xl cursor-pointer"
+						style="background-color: {shape.color}"
+					>
+					</div>
+				</a>
 			{/each}
 		</div>
 	</div>
@@ -43,6 +76,9 @@
 	<!--Sign-->
 	<div class="absolute bottom-0 right-0 w-52">
 		<img
+			data-aos="fade"
+			data-aos-delay={1200}
+			data-aos-duration={750}
 			src={sign}
 			alt="Signature"
 			class="object-fill pb-4 pr-4"
---
+++ src/routes/wavy/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<div class="dark h-screen w-screen bg-[#121212]">
+
+</div>
