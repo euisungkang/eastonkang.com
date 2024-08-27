3ccdbe8c7dc54a0defec3dcf4d11e0e047f9f10f
euisungkang <euisungkang@gmail.com>
2024-08-27 23:17:31 +0900

    feat: custom fonts and signature import

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'aston': ['"Aston"', 'sans-serif'],
+        'inter': ['"Inter"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
