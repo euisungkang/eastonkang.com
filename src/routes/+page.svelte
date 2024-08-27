40a9069f6da5b187da8f6dc0e5d83e96418b2d9b
euisungkang <euisungkang@gmail.com>
2024-08-27 23:17:53 +0900

    feat: home revamp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import sign from "$lib/assets/sign-min.png";
+	import AOS from "aos";
+
+	AOS.init();
+
+	const shapes = [
+		{
+			color: '#8E7AB5'
+		},
+		{
+			color: '#B784B7'
+		},
+		{
+			color: '#E493B3'
+		},
+		{
+			color: '#EEA5A6'
+		},
+		{
+			color: '#8E7AB5'
+		},
+		{
+			color: '#B784B7'
+		},
+	];
+</script>
+
+<div class="dark h-screen w-screen flex items-center justify-center bg-[#121212]">
+	<div class="w-full h-full flex items-center justify-center">
+		<div class="grid grid-cols-3 place-items-center place-content-center">
+			{#each shapes as shape}
+				<div
+					class="h-52 w-52 col-span-1 m-4 rounded-xl cursor-pointer
+						 transition ease-in-out duration-200 hover:scale-110"
+					style="background-color: {shape.color}"
+				>
+				</div>
+			{/each}
+		</div>
+	</div>
+
+	<!--Sign-->
+	<div class="absolute bottom-0 right-0 w-52">
+		<img
+			src={sign}
+			alt="Signature"
+			class="object-fill pb-4 pr-4"
+		/>
+<!--		<div class="absolute text-right bottom-0 right-0 pb-4 pr-6 font-aston text-gray-200 text-xs">-->
+<!--			Easton K.-->
+<!--		</div>-->
+	</div>
+</div>
---
+++ src/routes/exp/+page.svelte
@@ -0,0 +1,59 @@
+<!--<script>-->
+<!--	import {draw} from 'svelte/transition';-->
+
+<!--	let commands = [-->
+<!--		'M 0 40 h50',-->
+<!--		'M 50 30 h-50',-->
+<!--		'M 0 20 h50',-->
+<!--		'M 50 10 h-50'-->
+<!--	];-->
+<!--	const max = 50;-->
+<!--	let show = true;-->
+
+<!--	function onKeyDown(e) {-->
+<!--		switch(e.keyCode) {-->
+<!--			case 39:-->
+<!--				show = !show;-->
+<!--				break;-->
+<!--		}-->
+<!--	}-->
+<!--</script>-->
+
+<!--<div class="h-screen w-screen flex items-center justify-center bg-black">-->
+<!--	{#if show}-->
+<!--			<svg preserveAspectRatio="none" width={"100%"} height={"100%"} viewBox="0 0 {max} {max}">-->
+<!--				<g transform="translate(0 {max}) scale(1 -1)">-->
+<!--					<path transition:draw={{duration: 700}}-->
+<!--								d={commands[0]}-->
+<!--								fill="none"-->
+<!--								stroke="white"-->
+<!--								stroke-width="0.1px"-->
+<!--					/>-->
+<!--					<path transition:draw={{duration: 700}}-->
+<!--								d={commands[1]}-->
+<!--								fill="none"-->
+<!--								stroke="white"-->
+<!--								stroke-width="0.1px"-->
+<!--					/>-->
+<!--					<path transition:draw={{duration: 700}}-->
+<!--								d={commands[2]}-->
+<!--								fill="none"-->
+<!--								stroke="white"-->
+<!--								stroke-width="0.1px"-->
+<!--					/>-->
+<!--					<path transition:draw={{duration: 700}}-->
+<!--								d={commands[3]}-->
+<!--								fill="none"-->
+<!--								stroke="white"-->
+<!--								stroke-width="0.1px"-->
+<!--					/>-->
+<!--				</g>-->
+<!--			</svg>-->
+<!--	{/if}-->
+<!--</div>-->
+
+<!--<svelte:window on:keydown|preventDefault={onKeyDown} />-->
+
+<!--<style>-->
+
+<!--</style>-->
