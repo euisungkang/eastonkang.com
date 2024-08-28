5b7db9d7af7ec558130aab878f8bd7b8a80ea137
euisungkang <euisungkang@gmail.com>
2024-08-28 23:58:26 +0900

    fix: touch up on color palette

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import "../app.css";
 </script>
 
-<slot />
+<div class="hidden lg:block">
+	<slot />
+</div>
+<div class="dark bg-[#121212] h-screen w-screen flex items-center justify-center text-center lg:hidden">
+	Mobile Not Supported
+</div>
