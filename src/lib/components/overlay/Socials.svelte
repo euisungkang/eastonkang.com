0a3595be38ef7952c7f3832b9c397ed07c999792
euisungkang <euisungkang@gmail.com>
2025-04-01 01:32:25 -0400

    feat: color transition, overlay changes, modularization of static

---
+++ src/lib/components/overlay/Socials.svelte
@@ -0,0 +1,29 @@
+<script lang='ts'>
+  let { 
+    overlayColor = '#FFFFFF'
+  }: {
+    overlayColor: string,
+  } = $props();
+</script>
+
+<div
+  class="absolute font-jws z-10 text-xs bottom-12 right-12 flex flex-col items-end"
+  style:color={overlayColor}
+>
+  <a
+    aria-label="LinkedIn"
+    href="https://www.linkedin.com/in/easton/"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    LINKEDIN
+  </a>
+  <a
+    aria-label="Github"
+    href="https://github.com/euisungkang"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    GITHUB
+  </a>
+</div>
