76021212465ed1041943cad78fe5732ec7d5d43f
euisungkang <euisungkang@gmail.com>
2025-04-01 22:37:54 -0400

    docs: clean tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-// import plugin from 'tailwindcss/plugin';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -20,18 +18,7 @@ export default {
   },
   plugins: [
     // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('@tailwindcss/typography'),
-    // plugin(function({ matchUtilities, theme }) {
-    //   matchUtilities(
-    //     {
-    //       'translate-z': (value) => ({
-    //         '--tw-translate-z': value,
-    //         transform: ` translate3d(var(--tw-translate-x), var(--tw-translate-y), var(--tw-translate-z)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))`,
-    //       }), // this is actual CSS
-    //     },
-    //     { values: theme('translate'), supportsNegativeValues: true }
-    //   )
-    // })
+    require('@tailwindcss/typography')
   ],
 }
 
