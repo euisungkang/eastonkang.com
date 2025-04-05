f31735bda2f340cd540c0fcdec70571eab185aa2
euisungkang <euisungkang@gmail.com>
2025-04-05 12:59:52 -0400

    docs: HTML title, favicon update

--- src/lib/components/overlay/Logo.svelte
+++ src/lib/components/overlay/Logo.svelte
@@ -12,36 +12,11 @@
 
 <a
   aria-label="Home Logo Button"
-  class="absolute font-tny text-5xl top-8 left-12"
-  href="/gallery"
+  class="absolute font-tny text-5xl top-8 left-12 z-10"
+  href="/"
   style:color={overlayColor}
 >
   <TextSlideX
     text={text}
   />
 </a>
-
-  <!-- onmouseover={() => glitchText()} -->
-  <!-- onfocus={() => {}} -->
-
-  <!-- // const letters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; -->
-  <!-- // const original: string = 'EASTON'; -->
-  <!-- // let text: string = $state('HEUSON'); -->
-  <!-- // -->
-  <!-- // onMount(() => { -->
-  <!-- //   glitchText(); -->
-  <!-- // }); -->
-  <!-- // -->
-  <!-- // function glitchText() { -->
-  <!-- //   let iterations = 0; -->
-  <!-- //   const interval = setInterval(() => { -->
-  <!-- //     text = text.split('') -->
-  <!-- //                .map((l, i) => { -->
-  <!-- //                  if (i < iterations) return original[i]; -->
-  <!-- //                  return letters[Math.floor(Math.random() * 26)]; -->
-  <!-- //                }) -->
-  <!-- //                .join(''); -->
-  <!-- //     if (iterations >= original.length) clearInterval(interval); -->
-  <!-- //     iterations += 1 / 5; -->
-  <!-- //   }, 30); -->
-  <!-- // } -->
