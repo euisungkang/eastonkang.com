cef47fcd3cd80f5fb6caa9aa24bc6723562fdcf7
euisungkang <euisungkang@gmail.com>
2025-09-07 22:03:39 -0500

    feat: reset default open

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -83,7 +83,7 @@
 			trackVisible = true;
 			expandImage(selectedIndex);
 			console.log('<Developed by Easton Kang> https://eastonkang.com');
-			expandImage(6);
+			// expandImage(6);
 		}, 500);
 	});
 </script>
